971c69c464db36ac2facb603733cad85e058a9b7
Eliasj18 <eliasnjoyas@gmail.com>
2023-05-10 16:21:56 -0300

    MusicAPI finalizada

---
+++ Entities/Artista.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace MusicApi.Entities
+{
+    public class Artista
+    {
+       public int Id { get; set; }
+       [Required]
+       public string Name { get; set; }
+       public string Image { get; set; }
+       public List<Cancion> Canciones { get; set; }
+    }
+}
---
+++ Migrations/20230509224139_imageAdded.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicApi.Migrations
+{
+    public partial class imageAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Canciones_Artistas_ArtistaId",
+                table: "Canciones");
+
+            migrationBuilder.DropColumn(
+                name: "Genero",
+                table: "Canciones");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistaId",
+                table: "Canciones",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Artistas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Canciones_Artistas_ArtistaId",
+                table: "Canciones",
+                column: "ArtistaId",
+                principalTable: "Artistas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Canciones_Artistas_ArtistaId",
+                table: "Canciones");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Artistas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArtistaId",
+                table: "Canciones",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genero",
+                table: "Canciones",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Canciones_Artistas_ArtistaId",
+                table: "Canciones",
+                column: "ArtistaId",
+                principalTable: "Artistas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
